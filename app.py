3fcc232498fc40ac8fa55a622c1fe611b0a45432
ssabrut <michael.gunawan2002@gmail.com>
2023-12-06 08:41:59 +0700

    Fix CUDA devices

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 model_name_or_path = "TheBloke/Llama-2-7b-Chat-GPTQ"
-model = AutoModelForCausalLM.from_pretrained(
-    model_name_or_path, device_map="auto", trust_remote_code=False, revision="main"
-)
+print("[INFO] Loading model from HuggingFace model hub...")
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="cuda")
 
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
 
-prompt = "Hello, how are you?"
-prompt_template = f"""[INST]<<SYS>>
+prompt = "Hello how are you?"
+prompt_template = f"""[INST] <<SYS>>
 You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
 <</SYS>>
 {prompt}[/INST]
 """
 
-print("\n\n*** Generate:")
-
-input_ids = tokenizer(prompt_template, return_tensors="pt").input_ids.cuda()
-output = model.generate(
-    inputs=input_ids,
-    temperature=0.7,
-    do_sample=True,
-    top_p=0.95,
-    top_k=40,
-    max_new_tokens=512,
-)
-print(tokenizer.decode(output[0]))
-
-print("*** Pipeline:")
+print("[INFO] Generating response...")
 pipe = pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
     max_new_tokens=512,
     do_sample=True,
-    temperature=0.7,
+    temperature=0.1,
     top_p=0.95,
     top_k=40,
     repetition_penalty=1.1,
