b5dd6eac6ca5ee13739546ad3fe8eaefbf255b61
ssabrut <michael.gunawan2002@gmail.com>
2023-12-06 13:35:46 +0700

    Define languages constraint for agent

--- app.py
+++ app.py
@@ -5,13 +5,7 @@ print("[INFO] Loading model from HuggingFace model hub...")
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="cuda")
 
-
-prompt = "Hello how are you?"
-prompt_template = f"""[INST] <<SYS>>
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-<</SYS>>
-{prompt}[/INST]
-"""
+prompt_template = f"""Write a simple code for website using HTML"""
 
 print("[INFO] Generating response...")
 pipe = pipeline(
---
+++ src/data/constraint_languages.py
@@ -0,0 +1,5 @@
+CONSTRAINT_LANGUAGES = [
+    {"name": "HTML", "extensions": [".html", ".htm"], "node_name": "html"},
+    {"name": "CSS", "extensions": [".css"], "node_name": "css"},
+    {"name": "Javascrip", "extensions": [".js"], "node_name": "javascript"},
+]
